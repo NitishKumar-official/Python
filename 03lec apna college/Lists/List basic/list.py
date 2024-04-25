46dcfb82f4b1c19f3bd20ef697d948264d67ec07
nitish <kumarnitish27212@gmail.com>
2024-04-25 23:30:14 +0530

    list slicing and all

---
+++ 03lec apna college/Lists/List basic/list.py
@@ -0,0 +1,20 @@
+
+
+Marks = [25,3,6,8,95,54,565,565,]
+
+print(Marks)                      #for display all numbers
+print(type(Marks))                # to cheak the type of marks
+print(Marks[2])                   #we can also print any particular element using inde
+print("len:", len(Marks))         # we can also print the length of sets
+
+# Note :- a built in data tpye that stores set of vales
+         # it can store elements of different types(interger float string etc)
+
+student = ["karan", 95.4, 17, "delhi"]
+print(student)         
+
+#Note:- string me hm bss indexing se character ko access kr pate the pr usme change nhi kr pate the 
+       #lekin set me hm dono kam kr skte hain ek sath
+
+student[0]= "rana"
+print(student)       
---
+++ 03lec apna college/Lists/list slicing/list_slicing.py
@@ -0,0 +1,11 @@
+ # it is similar to the string slicing
+
+marks = [34, 35, 67, 89, 45, 54]
+print(marks[1:3])
+print(marks[1:])
+print(marks[:3])
+print(marks[:len(marks)])
+
+#negative indexing
+
+print(marks[-3:-1])
